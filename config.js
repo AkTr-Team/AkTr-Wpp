3fee9f89dcb9aa421ec791b7bcff1513fc57c372
AkTr_ <90263361+AkTr-Team@users.noreply.github.com>
2021-09-07 23:35:31 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,39 @@
+Olá! sou AkTr, Editei Essa Script No Intuito de
+Ajudar Vocês a Desbanir/Derrubar/Blindar/Desblindar
+Número :/
+
+Créditos: Kiny
+pois foi ele que fez a script, eu (aktr) apenas
+editei ela :)
+
+Por Favor, Deixe Seu Nick Aqui:
+
+nickuser
+
+SE Não Sabe Modificar No Nano Config
+Tem Uma Função de 'repleace' algo assim,
+troque o texto 'nickuser' e coloque teu nick
+
+Como Instala o Script?
+
+comandos:
+apt update && apt upgrade
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+git clone https://github.com/AkTr-Team/AkTr-Wpp
+cd AkTr-Wpp
+ls
+python3 main.py
+
+Digite o "8" e der Enter;
+Ative a Função 'apps menos seguros' na tua conta;
+Escolha o Método;
+Coloque o Número;
+Coloque Teu Email/Senha;
+Quantidade de Emails (recomendado 1 a 5, mais 1)
+e Esperar Resposta ao suporte.
+
+//by AkTr :)
+
+
