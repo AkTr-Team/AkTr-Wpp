66967cad124dd3965920b54f08b4c232a19dd94d
AkTr_ <90263361+AkTr-Team@users.noreply.github.com>
2021-09-08 22:27:51 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
-echo "[*] Instalando Dependências, Aguarde... [*]
+clear
+echo "Instalando Dependências, Aguarde...{C} [*]
 
+
+"
+echo "[*]
+
+Sempre Que Querer Usar o Script de Novo, use o Comando 'bash install.sh'
 
 "
 apt-get update && apt-get upgrade
@@ -7,6 +13,7 @@ apt-get install git -y
 apt-get install nodejs-lts -y
 pip install requests
 pkg install python -y && pkg install python2 -y && pkg install python3 -y
+clear
 
 echo "
 
